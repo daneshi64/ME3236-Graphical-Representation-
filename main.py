2bcbf81fde443ffcfdf4d9653c5ee78c9ab1ad6f
daneshi64 <daneshi64@gmail.com>
2026-09-24 22:41:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from io import BytesIO
 # ============================================================
 
 st.set_page_config(
-    page_title="Statistical Plotting Tool",
+    page_title="Descriptive Statistics & Graphical Representation",
     layout="centered"
 )
 
